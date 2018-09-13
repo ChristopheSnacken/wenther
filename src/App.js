9ca5c6b3ada79d4bab966eb031fba20ea52630f0
ChristopheSnacken <c.snacken@gmail.com>
2018-09-13 20:43:42 +0200

    implemented some extra features - small problem with refresh

--- src/App.js
+++ src/App.js
@@ -8,31 +8,73 @@ import Weather from './components/Weather'
 
   class App extends React.Component {
 
+
     state = {
-      temperature: undefined,
+      all: undefined,
       city: undefined,
       country: undefined,
-      humidity: undefined,
-      description: undefined,
       error: undefined
     }
+
+    componentDidMount() {
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+
+      function success(pos) {
+        var crd = pos.coords;
+
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${crd.latitude}&lon=${crd.longitude}&appid=c6b4119974f75434fad9c40a13358dc5`)
+          .then(res => res.json())
+          .then(
+            (response) => {
+              console.log(response)
+              this.setState({
+                all: response.list,
+                city: response.city.name,
+                country: response.city.country,
+                error: ""
+              });
+            },
+
+            (error) => {
+              this.setState({
+                error: "Could not read your location",
+                error
+              });
+            }
+          )
+        }
+
+
+
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+
+      navigator.geolocation.getCurrentPosition(success, error, options);
+
+}
+
+
+
 
     getWeather = async (e) => {
 
       e.preventDefault();
       const city = e.target.elements.city.value;
       const country = e.target.elements.country.value;
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=c6b4119974f75434fad9c40a13358dc5`);
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&units=metric&appid=c6b4119974f75434fad9c40a13358dc5`);
       const response = await api_call.json();
       console.log(response);
 
       if(city && country){
         this.setState({
-          temperature: response.main.temp,
-          city: response.name,
-          country: response.sys.country,
-          humidity: response.main.humidity,
-          description: response.weather[0].description,
+          all: response.list,
+          city: response.city.name,
+          country: response.city.country,
           error: ""
         })
       }else{
@@ -52,8 +94,7 @@ import Weather from './components/Weather'
        <Weather temperature={this.state.temperature}
           city={this.state.city}
           country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
+          all={this.state.all}
           error={this.state.error}/>
      </div>
     )
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,16 +1,43 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
 
 class Weather extends React.Component{
-  
+
+
   render(){
+
+
     return(
       <div>
-        {this.props.country && this.props.city && <p>Location: {this.props.city}, {this.props.country}</p> }
-        {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-        {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
-        {this.props.description && <p>Conditions:  {this.props.description}</p>}
-        {this.props.error && <p>{this.props.error}</p>}
+
+          {this.props.country && this.props.city && <p><b>{this.props.city}, {this.props.country}</b></p> }
+
+          {this.props.all && this.props.all.map(x =>
+            <Card>
+              <CardContent>
+                <Typography color="textSecondary">
+                  {x.dt_txt}
+                </Typography>
+                <Typography>
+
+                  <img src={`http://openweathermap.org/img/w/${x.weather[0].icon}.png`} alt="icon"/>
+                </Typography>
+                <Typography variant="headline" component="h2">
+                  {x.main.temp} <i>&deg;C</i>
+                </Typography>
+                <Typography component="p">
+                  Humidity: {x.main.humidity} <i>%</i>
+                </Typography>
+              </CardContent>
+            </Card>)}
+
+          {this.props.error && <p>{this.props.error}</p>}
+
       </div>
+
+
     )
   }
 }
